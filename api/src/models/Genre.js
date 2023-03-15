ae3527fe32aff95ddf2fca5734aa23c9527ec539
Lucas <lucasregner2002@gmail.com>
2023-03-15 01:18:46 -0300

    genres fixed 3.0

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -1,35 +1,43 @@
-const { Router } = require("express");
-const { Videogame, Genre } = require("../db");
-const { api_key } = process.env;
-const router = Router();
-const axios = require("axios");
-const {
-  OK,
-  CREATED,
-  BAD_REQUEST,
-  NOT_FOUND,
-} = require("../controllers/status");
-
-router.get("/", async (_req, res) => {
-  try {
-    const apigenres = (
-      await axios.get(`https://api.rawg.io/api/genres?key=${api_key}`)
-    ).data.results;
-    const apiGen = apigenres;
-    apiGen.map(
-      async (e) =>
-        await Genre.findOrCreate({
-          where: {
-            id: e.id,
-            name: e.name,
-          },
-        })
-    );
-    const genreDb = await Genre.findAll();
-    res.status(OK).send(genreDb);
-  } catch (error) {
-    res.status(NOT_FOUND).send("Not found");
-  }
-});
-
-module.exports = router;
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "genre",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.ENUM(
+          "Action",
+          "Indie",
+          "Adventure",
+          "RPG",
+          "Strategy",
+          "Shooter",
+          "Casual",
+          "Simulation",
+          "Puzzle",
+          "Arcade",
+          "Platformer",
+          "Racing",
+          "Massively Multiplayer",
+          "Sports",
+          "Fighting",
+          "Family",
+          "Board Games",
+          "Educational",
+          "Card"
+        ),
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
