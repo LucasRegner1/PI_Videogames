dfee574883e7ea5c4826c9589f7c89d4ea3dfb22
Lucas <lucasregner2002@gmail.com>
2023-03-20 14:42:26 -0300

    bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
-import Detail from "./components/Detail";
-import Form from "./components/Form";
+import Description from "./components/Description/Description";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/videogames" component={Home} />
-          <Route exact path="/videogames/:id" component={Detail} />
-          <Route exact path="/form" component={Form} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/videogames" component={Home} />
+        <Route exact path="/videogames/:id" component={Description} />
+        <Route exact path="/form" component={Form} />
+      </Switch>
+    </div>
   );
 }
 
