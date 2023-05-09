840f180d5d6703ab572cd36181f8645e000c09f1
Lucas <lucasregner2002@gmail.com>
2023-05-09 11:24:56 -0300

    ultimos cambios deploy

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,18 +5,6 @@ function Card({ name, image, genres, id, rating, createdInDb }) {
   console.log(name, image, genres, id, rating);
   let genres2 = genres.map((e) => (e.name ? e.name : e));
   return (
-    // <div className={s.card}>
-    //   <div>
-    //     <p>{rating}</p>
-    //     <img alt="imagen del juego" src={image} />
-    //     <div>
-    //       <div>
-    //         <p> {genres2.join(", ")}</p>
-    //       </div>
-    //       <h3>{name}</h3>
-    //     </div>
-    //   </div>
-    // </div>
     <div className={s.body}>
       <div className={s.card}>
         <img src={image} alt="imagen del juego" />
